f89cd623d555de5cad45c5a1a1e0599e5cf298a0
berkeskr <berkeskr@gmail.com>
2022-08-09 14:48:29 +0300

    Admin model & controller & service & repo added

---
+++ src/main/java/com/example/creditapplicationsystem/service/AdminService.java
@@ -0,0 +1,48 @@
+package com.example.creditapplicationsystem.service;
+
+import com.example.creditapplicationsystem.model.Admin;
+import com.example.creditapplicationsystem.repository.AdminRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    private AdminRepo adminRepo;
+
+    public List<Admin> getAllAdmins(){
+        List<Admin> allAdmins = adminRepo.findAll();
+        return allAdmins;
+    }
+
+    public Optional<Admin> getAdminByID(Long ID){
+        Optional<Admin> admin = adminRepo.findById(ID);
+        return admin;
+    }
+
+
+    public Admin createAdmin(Admin admin){
+        return adminRepo.save(admin);
+
+    }
+
+    public void deleteAdmin(Long id){
+        adminRepo.deleteById(id);
+    }
+
+    public Admin updateAdmin(Long id, Admin admin){
+        Optional<Admin> existingAdmin = adminRepo.findById(id);
+        existingAdmin.get().setName(admin.getName());
+        existingAdmin.get().setSurname(admin.getSurname());
+
+        return adminRepo.save(existingAdmin.get());
+    }
+
+
+
+
+}
