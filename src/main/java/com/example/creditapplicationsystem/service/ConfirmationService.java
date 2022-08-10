99d6ca5d83b22672e18da1be8d43638441bf78e7
berkeskr <berkeskr@gmail.com>
2022-08-10 23:30:25 +0300

    Credit Confirmation Service added

---
+++ src/main/java/com/example/creditapplicationsystem/service/ConfirmationService.java
@@ -0,0 +1,33 @@
+package com.example.creditapplicationsystem.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConfirmationService {
+
+    private static final int creditLimitFactor=4;
+
+    public String status(int creditScore){
+        if(creditScore<500){
+            return "Rejected!";
+        }else {
+            return "Accepted";
+        }
+
+    }
+
+    public int calculateLimit(int creditScore, double salary){
+        if(creditScore<500){
+            return 0;
+        }else if(creditScore<1000&&creditScore>=500){
+            if (salary<5000.0){
+                return 10000;
+            }else{
+                return 20000;
+            }
+        }else{
+            return (int) (salary*creditLimitFactor);
+        }
+    }
+
+}
